95859d46425806ebedac65fca9c0ad73519854f1
Jinchao <liujc0208@163.com>
2023-11-05 21:05:39 +0800

    规范点了

--- multi_compare.py
+++ multi_compare.py
@@ -1,16 +1,12 @@
 """ 
-author:jinchao
-info: used for muilt file to detect temp
+used for muilt file to detect temp
  """
 import matplotlib.pyplot as plt
 from scipy.ndimage import gaussian_filter1d
 from scipy.interpolate import make_interp_spline
 import os
 
-#初始化全局变量
 
-
-#求二位数组的第二维度长度最小值
 def find_min_len_of_sec_dementia( Temps ):
     lens_temp=[]
     for i in range(len(Temps)):
@@ -19,14 +15,15 @@ def find_min_len_of_sec_dementia( Temps ):
     max_lenvalue = max(lens_temp) # 求列表最大值
     return min_lenvalue
 
-#寻找txt文件
+
 def find_txt_file_in_now_dir( filename ):
     datanames = os.listdir()
     for dataname in datanames:
         if os.path.splitext(dataname)[1] == '.txt':#目录下包含.txt的文件
             filename.append(dataname)    
     print(filename)
-#提取txt文件信息
+
+
 def extract_txt_info(filename,Counts,Temps):
     for file in filename:
         with open(file, 'r') as f:
@@ -45,8 +42,7 @@ def extract_txt_info(filename,Counts,Temps):
             Temps.append(temp)
 
 
-
-if __name__ == '__main__':
+def run():
     aa =[1,2,3]
     aa =1
     Counts=[]
@@ -58,7 +54,7 @@ if __name__ == '__main__':
     extract_txt_info(filename,Counts,Temps)
     min_lenvalue=find_min_len_of_sec_dementia(Temps)
 
-    #画图
+
     for i in range(len(Temps)):
         Temps_out=Temps[len(Temps)-i-1][0:min_lenvalue]
         Counts_out=Counts[len(Temps)-i-1][0:min_lenvalue]
@@ -66,4 +62,5 @@ if __name__ == '__main__':
     plt.legend()
     plt.show()
 
-
+if __name__ == '__main__':
+    run()
