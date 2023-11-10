b6007e98aab1b07d07e904208e1d7dddf368addf
Jinchao <liujc0208@163.com>
2023-11-10 11:09:56 +0800

    bug fix

--- multi_fig.py
+++ multi_fig.py
@@ -19,28 +19,24 @@ def find_min_len_of_sec_dementia( Temps ):
 
 def find_txt_file_in_now_dir( filename ):
     datanames = os.listdir()
-    #datanames.sort()#乱序问题进行解决
     for dataname in datanames:
         if os.path.splitext(dataname)[1] == '.txt':#目录下包含.txt的文件
             filename.append(dataname) 
     filename.sort() #乱序问题进行解决          
     #print(filename)
 
-
-def extract_txt_info(filename,Temps):
+def extract_info(filename,Temps):
     for file in filename:
         with open(file, 'r') as f:
             lines = f.readlines()
-            count = 0
             temp=[]
             for line in lines:
-                count +=1
-                if count%19 ==17:
+                if "温度" in line:
                     valuee = line.split()
                     temp1=float(valuee[1][0:5])/1000
                     temp.append(temp1)
             Temps.append(temp)
-
+    print(len(Temps))
 
 def bar_with_date(Temps,filename):
     plt.rcParams['font.sans-serif'] = ['SimHei'] # 显示中文
@@ -50,9 +46,11 @@ def bar_with_date(Temps,filename):
         if len(Temps[i])<10:
             mean_temp.append(Temps[i][-1])
         else:
-            mean_temp.append(mean(Temps[i][-60:]))
+            mean_temp.append(mean(Temps[i][-15:]))
         print("item"+str(i)+":"+filename[i])
         filename[i]=str(i)
+        #print((mean_temp[i]))
+
     print("---------------")
     width = 0.5  # 柱体宽度
     plt.bar(filename, mean_temp, width,label='temp')
@@ -65,29 +63,6 @@ def bar_with_date(Temps,filename):
     #plt.legend()
     plt.show()
 
-
-def bar_with_date_sort(Temps,filename):
-    plt.rcParams['font.sans-serif'] = ['SimHei'] # 显示中文
-    mean_temp=[]
-    for i in range(len(Temps)):
-        if len(Temps[i])<10:
-            mean_temp.append(Temps[i][-1])
-        else:
-            mean_temp.append(mean(Temps[i][-10:]))
-        print(str(i)+":"+filename[i])
-        filename[i]=str(i)
-    width = 0.5  # 柱体宽度
-    plt.bar(filename, mean_temp, width,label='temp')
-    for a,b,i in zip(filename,mean_temp,range(len(filename))): # zip 函数
-        plt.text(a,b+0.13,"%.2f"%mean_temp[i],ha='center') # plt.text 函数
-       
-    plt.xlabel('测试项目编号')
-    plt.ylabel('温度/℃')
-    plt.title('温度稳态结果图./')
-    plt.legend()
-    plt.show()
-
-
 def process_fig(Temps,filename):
     min_lenvalue=find_min_len_of_sec_dementia(Temps)
     Counts_out=[x for x in range(min_lenvalue)]
@@ -97,12 +72,11 @@ def process_fig(Temps,filename):
     plt.legend()
     plt.show()
 
-
 def run():
     Temps=[]
     filename=[]
     find_txt_file_in_now_dir(filename)
-    extract_txt_info(filename,Temps)
+    extract_info(filename,Temps)
     #process_fig(Temps,filename)
     bar_with_date(Temps,filename)
 
